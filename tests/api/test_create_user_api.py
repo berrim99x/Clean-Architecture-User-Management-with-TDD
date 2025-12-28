b4564cbe22bf1cabc826cad45d30c589d4d65ccb
berrim99x <hakimhakim20046@gmail.com>
2025-12-28 22:45:07 +0100

    RED: add integration test for create user api

---
+++ tests/api/test_create_user_api.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+
+def test_create_user_endpoint():
+    client = TestClient(app)
+
+    response = client.post(
+        "/users",
+        json={
+            "first_name": "Abdelhakim",
+            "last_name": "Berrim",
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "full_name": "Abdelhakim Berrim"
+    }
