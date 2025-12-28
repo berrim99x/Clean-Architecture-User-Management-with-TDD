855aea731f3741bafda10b791da933a97fce442a
berrim99x <hakimhakim20046@gmail.com>
2025-12-28 21:08:40 +0100

    RED: test saving user passes user to repository

--- tests/test_saving_use_case.py
+++ tests/test_saving_use_case.py
@@ -15,3 +15,16 @@ def test_saving_user_is_calling_delegated_repository():
 
     # Assert
     spy_user_repository.save.assert_called_once()
+
+def test_saving_user_save_the_user_in_the_repository():
+    # Arrange
+    user = User(first_name="Abdelhakim", last_name="Berrim")
+    spy_user_repository = Mock(spec=UserRepositoryInterface)
+
+    saving_use_case = SavingUseCase(user_repository=spy_user_repository)
+
+    # Act
+    saving_use_case.execute(user)
+
+    # Assert
+    spy_user_repository.save.assert_called_once_with(user)
