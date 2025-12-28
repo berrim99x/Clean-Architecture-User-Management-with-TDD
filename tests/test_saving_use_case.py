8d6ce686dcdef929ae2a9b54b677b5a90330d844
berrim99x <hakimhakim20046@gmail.com>
2025-12-28 21:20:10 +0100

    RED: test saving user calls presenter

--- tests/test_saving_use_case.py
+++ tests/test_saving_use_case.py
@@ -39,3 +39,23 @@ def test_saving_user_save_the_user_in_the_repository(user):
 
     # Assert
     spy_user_repository.save.assert_called_once_with(user)
+
+
+def test_saving_user_calls_presenter():
+    # Arrange
+    user = User(first_name="Abdelhakim", last_name="Berrim")
+    spy_user_repository = Mock(spec=UserRepositoryInterface)
+    spy_presenter = Mock()
+
+    from src.use_cases.saving_use_case import SavingUseCase
+
+    saving_use_case = SavingUseCase(
+        user_repository=spy_user_repository,
+        presenter=spy_presenter,
+    )
+
+    # Act
+    saving_use_case.execute(user)
+
+    # Assert
+    spy_presenter.present.assert_called_once_with(user)
