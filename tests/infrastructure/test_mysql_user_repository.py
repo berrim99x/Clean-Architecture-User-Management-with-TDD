92a83adf0846e28813209f505a8b8cee6dcc2ecf
berrim99x <hakimhakim20046@gmail.com>
2025-12-28 22:36:37 +0100

    RED: test mysql user repository delegates save to db client

---
+++ tests/infrastructure/test_mysql_user_repository.py
@@ -0,0 +1,20 @@
+from unittest.mock import Mock
+
+from src.entities.user import User
+
+
+class MySQLUserRepository:
+    pass
+
+
+def test_mysql_user_repository_saves_user_using_db_client():
+    # Arrange
+    db_client = Mock()
+    repository = MySQLUserRepository(db_client=db_client)
+    user = User(first_name="Abdelhakim", last_name="Berrim")
+
+    # Act
+    repository.save(user)
+
+    # Assert
+    db_client.insert_user.assert_called_once_with(user)
