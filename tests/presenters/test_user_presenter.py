32400555b1da751fc00622de470f75e587329070
berrim99x <hakimhakim20046@gmail.com>
2025-12-28 22:03:30 +0100

    RED: test saving user presenter builds user view model

--- tests/presenters/test_user_presenter.py
+++ tests/presenters/test_user_presenter.py
@@ -14,3 +14,14 @@ def test_user_presenter_creates_view_model():
     # Assert
     assert isinstance(view_model, UserViewModel)
     assert view_model.full_name == "Abdelhakim Berrim"
+
+def test_presenter_builds_user_view_model_from_user():
+    # Arrange
+    presenter = SavingUserPresenter()
+    user = User(first_name="Abdelhakim", last_name="Berrim")
+
+    # Act
+    view_model: UserViewModel = presenter.present(user)
+
+    # Assert
+    assert view_model.full_name == "Abdelhakim Berrim"
