28efea919040115012e6cd67680e1cbd8213211d
berrim99x <hakimhakim20046@gmail.com>
2025-12-28 21:49:02 +0100

    GREEN: minimal user presenter implementation

--- tests/presenters/test_user_presenter.py
+++ tests/presenters/test_user_presenter.py
@@ -1,12 +1,15 @@
 from src.entities.user import User
 
+class UserViewModel:
+    def __init__(self, full_name: str):
+        self.full_name = full_name
+
+
 
 class UserPresenter:
-    pass
-
-
-class UserViewModel:
-    pass
+    def present(self, user: User) -> UserViewModel:
+        full_name = f"{user.first_name} {user.last_name}"
+        return UserViewModel(full_name=full_name)
 
 
 def test_user_presenter_creates_view_model():
