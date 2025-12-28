e992c53e43b50bf69973e0897099ebdaff0101db
berrim99x <hakimhakim20046@gmail.com>
2025-12-28 21:38:54 +0100

    GREEN: minimal saving user controller implementation

--- tests/controllers/test_saving_user_controller.py
+++ tests/controllers/test_saving_user_controller.py
@@ -1,7 +1,14 @@
 from unittest.mock import Mock
 from src.entities.user import User
 from src.use_cases.saving_use_case import SavingUseCase
-from src.controllers.saving_user_controller import SavingUserController
+
+class SavingUserController:
+    def __init__(self, saving_use_case):
+        self.saving_use_case = saving_use_case
+
+    def handle(self, first_name: str, last_name: str) -> None:
+        user = User(first_name=first_name, last_name=last_name)
+        self.saving_use_case.execute(user)
 
 
 def test_controller_calls_saving_use_case_with_user():
