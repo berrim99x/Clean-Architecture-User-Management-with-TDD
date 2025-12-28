ae9d170c37b1f833da4cfa916e60a730c3b7181b
berrim99x <hakimhakim20046@gmail.com>
2025-12-28 21:13:10 +0100

    BLUE: refactoring

--- tests/test_saving_use_case.py
+++ tests/test_saving_use_case.py
@@ -1,4 +1,7 @@
 from unittest.mock import Mock
+
+import pytest
+
 from src.entities.user import User
 from src.repositories.user_repository_interface import UserRepositoryInterface
 from src.use_cases.saving_use_case import SavingUseCase
@@ -16,9 +19,17 @@ def test_saving_user_is_calling_delegated_repository():
     # Assert
     spy_user_repository.save.assert_called_once()
 
-def test_saving_user_save_the_user_in_the_repository():
+@pytest.mark.parametrize(
+    "user",
+    [
+        User("Abdelhakim", "Berrim"),
+        User("Islam", "Hala"),
+        User("Oussama", "Khelef"),
+        User("Ahmed", "Zellouma"),
+    ],
+)
+def test_saving_user_save_the_user_in_the_repository(user):
     # Arrange
-    user = User(first_name="Abdelhakim", last_name="Berrim")
     spy_user_repository = Mock(spec=UserRepositoryInterface)
 
     saving_use_case = SavingUseCase(user_repository=spy_user_repository)
